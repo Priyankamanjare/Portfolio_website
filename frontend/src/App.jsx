b9b4495084eb1f13f43fc807d9b5bd2b4669bc7d
Priyanka Manjare <priyankamanjare05@gmail.com>
2025-09-22 12:06:56 +0530

    Changed bg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,11 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="relative min-h-screen w-full md:px-40 px-12 bg-gradient-to-br from-[#1a0033] via-[#140021] to-[#0f001a] overflow-hidden">
+    <div className="relative min-h-screen w-full md:px-40 px-12 bg-gradient-to-br from-[#160427] via-[#060109] to-[#000000] overflow-hidden">
       
       {/* Glow Elements */}
-      <div className="absolute top-20 left-10 w-[400px] h-[400px] bg-purple-700 rounded-full blur-[150px] opacity-40"></div>
-      <div className="absolute bottom-20 right-10 w-[500px] h-[500px] bg-fuchsia-600 rounded-full blur-[200px] opacity-30"></div>
+      <div className="absolute top-20 left-10 w-[400px] h-[400px] bg-fuchsia-950 rounded-full blur-[150px] opacity-40"></div>
+      <div className="absolute bottom-20 right-10 w-[500px] h-[500px] bg-fuchsia-950 rounded-full blur-[200px] opacity-30"></div>
         <Navbar />     
         <Profile />
         <Experience/>
