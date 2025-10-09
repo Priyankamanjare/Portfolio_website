c7a545655ec25c2be274d6c8d4dfb8f4ae1f80fe
Priyanka Manjare <priyankamanjare05@gmail.com>
2025-10-09 12:11:09 +0530

    Changed Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
     setShowNotification(true);
 
     try {
-      const response = await fetch("https://portfolio-website-2-mnti.onrender.com/contact", {
+      const response = await fetch("https://portfolio-website-w5jl.onrender.com/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
